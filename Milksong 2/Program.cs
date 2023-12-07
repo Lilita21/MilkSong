724f6e4335a9cf813d8fb08328f5b85be68d5d27
Lilita21 <151826227+Lilita21@users.noreply.github.com>
2023-12-07 13:37:27 +0200

    first commit

---
+++ Milksong 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace Milksong_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int milksleft = 99;
+            while (milksleft > 2)
+            {
+                Console.WriteLine ( milksleft + " bottles of milk on the wall, " + milksleft + " bottles of milk ");
+                milksleft--;
+                Console.WriteLine( "Take one down and pass it around , " + milksleft + " Bottles of milk ");
+                
+
+            }
+            Console.WriteLine(milksleft + " bottles of milk on the wall, " + milksleft + " bottles of milk ");
+            milksleft--;
+            Console.WriteLine("Take one down and pass it around , " + milksleft + " Bottle of milk ");
+
+            Console.WriteLine(milksleft + " bottle of milk on the wall, " + milksleft + " bottle of milk ");
+        }
+    }
+}
